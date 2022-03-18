996325d9b7bafd64591cebc5d3ed63b058fd01f0
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2022-03-18 22:17:00 +0100

    Added help entries for rootmenu

--- Snake/assets/menu/rootmenu/help.cmd
+++ Snake/assets/menu/rootmenu/help.cmd
@@ -1,15 +1,27 @@
 set rootmenuHelpInput=%~1
-set lowercase=abcdefghijklmnopqrstuvwxyz
-set uppercase=ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 
-
-if "%rootmenuHelpInput%"=="directory" (
-      echo u gay
+if "%rootmenuHelpInput%"=="DIRECTORY" (
+      echo %translation.rootmenu.usage.directory%
       exit /b
 )
 
-if "%rootmenuHelpInput%"=="gay" (
+if "%rootmenuHelpInput%"=="EXIT" (
+      echo %translation.rootmenu.usage.exit%
+      exit /b
+)
+
+if "%rootmenuHelpInput%"=="NAME" (
+      echo %translation.rootmenu.usage.name%
+      exit /b
+)
+
+if "%rootmenuHelpInput%"=="CLEAR" (
+      echo %translation.rootmenu.usage.clear%
+      exit /b
+)
+
+if "%rootmenuHelpInput%"=="GAY" (
       echo u gay
       exit /b
 )
