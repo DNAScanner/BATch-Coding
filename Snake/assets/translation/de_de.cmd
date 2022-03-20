0a51ce18be9dd7f4fb7485815c46ecda3ebf6873
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2022-03-20 19:41:41 +0100

    Update de_de translation

--- Snake/assets/translation/de_de.cmd
+++ Snake/assets/translation/de_de.cmd
@@ -1,106 +1,126 @@
-set translation.title=Snake by DNA
+set "translation.title=Snake by DNA"
+set "translation.pressAnyKey=Druecke eine Taste, um weiter zu kommen"
 
-set translation.fullscreenMessage1=x===================================================================x
-set translation.fullscreenMessage2=I     Setze das Spiel ins Vollbild fuer das beste Spielerlebnis     I
-set translation.fullscreenMessage3=x===================================================================x
+set "translation.fullscreenMessage1=x===================================================================x"
+set "translation.fullscreenMessage2=I     Setze das Spiel ins Vollbild fuer das beste Spielerlebnis     I"
+set "translation.fullscreenMessage3=x===================================================================x"
 
-set translation.menu.greeting=Hey Spieler, lass uns doch deinen Namen wissen! Wenn wir deinen Namen kennen, fuehlt sich das Spiel sicherlich genuesslicher an. Geb hier einfach deinen Namen ein:
-set translation.menu.greetingHello=Hey,
-set translation.menu.greetingSubmitted1=Danke, dass du uns deinen Namen verraetst,
-set translation.menu.greetingSubmitted2=, ab jetzt viel Spass
-set translation.menu.greetingFirstTime1=, das ist dein erstes mal, hier zu sein. Viel Spass!
-set translation.menu.greetingNotFirstTime1=, das ist dein%col_green%
-set translation.menu.greetingNotFirstTime2=%col_reset%mal, hier zu sein. Viel Spass!
+::Rootmenu
+set "translation.rootmenu.information=Druecke %col_red%[Q]%col_reset% zum Verlassen oder %col_red%[E]%col_reset% um Befehle einzugeben"
+set "translation.rootmenu.commandlist=Bisher gibt es ein paar Befehle, mehr werden bald hinzugefügt:%col_red%"
+set "translation.rootmenu.commandNotFound=Der Befehl wurde nicht gefunden, vielleicht schaust du dir sonst noch einmal die Verwendung des Befehls an: %col_red%HELP"
+set "translation.rootmenu.setname=Setze den Namen zu"
+set "translation.rootmenu.directory=Der Pfad des Spiels ist:"
+set "translation.rootmenu.reset=Dein Name wurde erfolgreich zurückgesetzt"
 
-set translation.menu.information1=Druecke %col_red%[W]%col_reset% oder %col_red%[S]%col_reset% um %col_red%hoch%col_reset% oder %col_red%runter%col_reset% zu navigieren
-set translation.menu.information2=Druecke %col_red%[D]%col_reset% oder %col_red%[A]%col_reset% um die Auswahl zu bestaetigen
+set "translation.rootmenu.usage.directory=Zeigt dir den Pfad des Spiels. Verwendung: %col_red%DIRECTORY%col_reset%"
+set "translation.rootmenu.usage.exit=Verlässt das Rootmenu und kehrt zum Hauptmenue zurück. Verwendung: %col_red%EXIT%col_reset%"
+set "translation.rootmenu.usage.name=Setze deinen Namen zurueck: %col_red%NAME RESET%col_reset%. Aendere deinen Namen: %col_red%NAME SET [NAME]%col_reset%"
+set "translation.rootmenu.usage.clear=Löscht den Consolenverlauf. Verwendung: %col_red%CLEAR%col_reset%"
 
-set translation.menu.button.play1=x=======================x
-set translation.menu.button.play2=I     S p i e l e n     I
-set translation.menu.button.play3=x=======================x
+::Greeting
+set "translation.menu.greeting=Hey Spieler, wuerdest du uns deinen Namen verraten? Wuerden wir deinen Namen kennen, wuerde sich das Spiel sicherlich besser anfuehlen. Gebe ihn einfach hier ein:"
+set "translation.menu.greetingHello=Hey,"
+set "translation.menu.greetingSubmitted1=Danke, dass du uns deinen Namen wissen laesst,"
+set "translation.menu.greetingSubmitted2=, nun habe Spass beim Spielen unseres Spiels"
+set "translation.menu.greetingFirstTime1=, das ist dein erstes Mal hier zu sein, viel Spass"
+set "translation.menu.greetingNotFirstTime1=, das ist das%col_green%"
+set "translation.menu.greetingNotFirstTime2=%col_reset%Mal, dass du hier bist"
 
-set translation.menu.button.language1=x=========================x
-set translation.menu.button.language2=I     S p r a c h e       I
-set translation.menu.button.language3=x=========================x
+::Mainmenu
+set "translation.menu.information1=Druecke %col_red%[W]%col_reset% oder %col_red%[S]%col_reset% um %col_red%hoch%col_reset% oder %col_red%runter%col_reset% zu navigieren"
+set "translation.menu.information2=Druecke %col_red%[D]%col_reset% oder %col_red%[A]%col_reset% zum Bestaetigen"
 
-set translation.menu.button.exit1=x===========================x
-set translation.menu.button.exit2=I     V e r l a s s e n     I
-set translation.menu.button.exit3=x===========================x
+::Mainmenu Buttons
+set "translation.menu.button.play1=x=======================x"
+set "translation.menu.button.play2=I     S p i e l e n     I"
+set "translation.menu.button.play3=x=======================x"
 
+set "translation.menu.button.language1=x=======================x"
+set "translation.menu.button.language2=I     S p r a c h e     I"
+set "translation.menu.button.language3=x=======================x"
 
-set translation.menuLanguage.multipleAvailable1=Es gibt
-set translation.menuLanguage.multipleAvailable2=Sprachen:
-set translation.menuLanguage.singleAvailable1=Es gibt
-set translation.menuLanguage.singleAvailable2=Sprache:
-set translation.menuLanguage.promptLanguage=Auf welcher Sprache willst du spielen? Gebe %col_red%RELOAD%col_reset% ein, um nach neuen Sprachen zu suchen, %col_red%CANCEL%col_reset% um dieses Menu zu schliessen oder %col_red%REAPPLY%col_reset% um die Sprache neu zu laden
-set translation.menuLanguage.notFound=Sprache nicht gefunden, bitte versuche es erneut
-set translation.menuLanguage.reloading=Suche nach neuen Sprachen
-set translation.menuLanguage.cancel=Sprachauswahl verlassen, die aktuelle Sprache ist
-set translation.menuLanguage.switch=Wechsle zur neuen Sprache, bitte warten
+set "translation.menu.button.exit1=x===================x"
+set "translation.menu.button.exit2=I     Verlassen     I"
+set "translation.menu.button.exit3=x===================x"
 
-::We're no strangers to love
-::You know the rules and so do I
-::A full commitment's what I'm thinking of
-::You wouldn't get this from any other guy
+::Languagemenu
+set "translation.menuLanguage.multipleAvailable1=Es wurden"
+set "translation.menuLanguage.multipleAvailable2=Sprachen gefunden:"
+set "translation.menuLanguage.singleAvailable1=Es wurde"
+set "translation.menuLanguage.singleAvailable2=Sprache gefunden:"
+set "translation.menuLanguage.promptLanguage=Welche Sprache willst du nutzen? Gebe %col_red%RELOAD%col_reset% ein, um nach neuen Sprachen zu suchen, %col_red%CANCEL%col_reset% um das Menue zu verlassen oder %col_red%REAPPLY%col_reset% um die Sprache neu zu laden"
+set "translation.menuLanguage.notFound=Die Sprache konnte nicht gefunden werden, versuche es erneut"
+set "translation.menuLanguage.reloading=Suche nach neuen Sprache laeuft"
+set "translation.menuLanguage.cancel=Sprachauswahl beendet, weiter geht's mit"
+set "translation.menuLanguage.reapply=Neue laden der Sprache laeuft"
+set "translation.menuLanguage.switch=Die Sprache wird gewechselt, bitte warten"
 
-::I just wanna tell you how I'm feeling
-::Gotta make you understand
+::Rofl Astling - Gebe dich nie auf [Deutschland's hoch kwallitatives modell]
 
-::Never gonna give you up
-::Never gonna let you down
-::Never gonna run around and desert you
-::Never gonna make you cry
-::Never gonna say goodbye
-::Never gonna tell a lie and hurt you
+::Liebe ist uns nicht fremd
+::Du kennst die Regeln und ich auch
+::Ein volles Engagement ist das, woran ich denke
+::Das würdest du von keinem anderen Typen bekommen
 
-::We've known each other for so long
-::Your heart's been aching but you're too shy to say it
-::Inside we both know what's been going on
-::We know the game and we're gonna play it
-::And if you ask me how I'm feeling
-::Don't tell me you're too blind to see
+::Ich will dir nur sagen, wie ich mich fühle
+::Ich muss dich verstehen lassen
 
-::Never gonna give you up
-::Never gonna let you down
-::Never gonna run around and desert you
-::Never gonna make you cry
-::Never gonna say goodbye
-::Never gonna tell a lie and hurt you
+::Gebe dich nie auf
+::Ich werde dich nicht enttäuschen
+::Ich werde niemals herumrennen und dich verlassen
+::Ich werde dich niemals zum Weinen bringen
+::Ich werde niemals auf Wiedersehen sagen
+::Ich werde niemals eine Lüge erzählen und dich verletzen
 
-::Never gonna give you up
-::Never gonna let you down
-::Never gonna run around and desert you
-::Never gonna make you cry
-::Never gonna say goodbye
-::Never gonna tell a lie and hurt you
+::Wir kennen uns schon so lange
+::Dein Herz schmerzt, aber du bist zu schüchtern, es zu sagen
+::Drinnen wissen wir beide, was los ist
+::Wir kennen das Spiel und wir werden es spielen
+::Und wenn Sie mich fragen, wie ich mich fühle
+::Sag mir nicht, du bist zu blind zum Sehen
 
-::Never gonna give, never gonna give
-::(Give you up)
+::Gebe dich nie auf
+::Ich werde dich nicht enttäuschen
+::Ich werde niemals herumrennen und dich verlassen
+::Ich werde dich niemals zum Weinen bringen
+::Ich werde niemals auf Wiedersehen sagen
+::Ich werde niemals eine Lüge erzählen und dich verletzen
 
-::We've known each other for so long
-::Your heart's been aching but you're too shy to say it
-::Inside we both know what's been going on
-::We know the game and we're gonna play it
+::Gebe dich nie auf
+::Ich werde dich nicht enttäuschen
+::Ich werde niemals herumrennen und dich verlassen
+::Ich werde dich niemals zum Weinen bringen
+::Ich werde niemals auf Wiedersehen sagen
+::Ich werde niemals eine Lüge erzählen und dich verletzen
 
-::I just wanna tell you how I'm feeling
-::Gotta make you understand
+::Niemals geben, niemals geben
+::(Gib dich auf)
 
-::Never gonna give you up
-::Never gonna let you down
-::Never gonna run around and desert you
-::Never gonna make you cry
-::Never gonna say goodbye
-::Never gonna tell a lie and hurt you
+::Wir kennen uns schon so lange
+::Dein Herz schmerzt, aber du bist zu schüchtern, es zu sagen
+::Drinnen wissen wir beide, was los ist
+::Wir kennen das Spiel und wir werden es spielen
+::
+::Ich will dir nur sagen, wie ich mich fühle
+::Ich muss dich verstehen lassen
 
-::Never gonna give you up
-::Never gonna let you down
-::Never gonna run around and desert you
-::Never gonna make you cry
-::Never gonna say goodbye
-::Never gonna tell a lie and hurt you
+::Gebe dich nie auf
+::Ich werde dich nicht enttäuschen
+::Ich werde niemals herumrennen und dich verlassen
+::Ich werde dich niemals zum Weinen bringen
+::Ich werde niemals auf Wiedersehen sagen
+::Ich werde niemals eine Lüge erzählen und dich verletzen
 
-::Never gonna give you up
-::Never gonna let you down
-::Never gonna run around and desert you
-::Never gonna make you cry
-::Never gonna say goodbye
+::Gebe dich nie auf
+::Ich werde dich nicht enttäuschen
+::Ich werde niemals herumrennen und dich verlassen
+::Ich werde dich niemals zum Weinen bringen
+::Ich werde niemals auf Wiedersehen sagen
+::Ich werde niemals eine Lüge erzählen und dich verletzen
+
+::Gebe dich nie auf
+::Ich werde dich nicht enttäuschen
+::Ich werde niemals herumrennen und dich verlassen
+::Ich werde dich niemals zum Weinen bringen
+::Ich werde niemals auf Wiedersehen sagen
